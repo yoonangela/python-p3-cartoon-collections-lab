9ef59e1e095d30495e4952f6f601e982bb49a08c
Yeji-Yoon <angelayoon0102@gmail.com>
2025-03-12 15:42:11 -0700

    s

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,23 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(names):
+    num=1
+    for name in names:
+        print(f"{num}. {name}")
+        num=num+1
 
-def summon_captain_planet():
-    pass
+def summon_captain_planet(calls):
+    calls= [f"{call.title()}!" for call in calls]
+    return calls
+        
 
-def long_planeteer_calls():
-    pass
+def long_planeteer_calls(calls):
+    for call in calls:
+        if len(call)>4:
+            return True
+    return False
 
-def find_the_cheese():
-    pass
+def find_the_cheese(foods):
+    cheese=["cheddar","gouda","camembert"]
+    for food in foods:
+        if food in cheese:
+            return food
+    return None
